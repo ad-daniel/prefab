617e99f63a8a9e8d9176f86595b99de110efe1ca
ad-daniel <daniel.dias@epfl.ch>
2022-08-08 17:19:30 +0200

    add examples

---
+++ supervisor_import/controllers/supervisor_import/supervisor_import.py
@@ -0,0 +1,45 @@
+"""my_controller2 controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Supervisor
+
+# create the Robot instance.
+robot = Supervisor()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getDevice('motorname')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+rootNode = robot.getRoot()  # get root of the scene tree
+rootChildrenField = rootNode.getField('children')
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+i = 0
+while robot.step(timestep) != -1:
+    if i == 2:
+        rootChildrenField.importMFNodeFromString(-1, 'DEF AA Group { children [ Nao{} ] }')
+    if i == 5:
+        node = robot.getFromDef('AA')
+        node.remove();
+    if i == 10:    
+        rootChildrenField.importMFNodeFromString(-1, 'Nao{ translation -1 0 0 }')    
+
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    # rootChildrenField.importMFNodeFromString(-1, 'Nao{}')
+
+    i += 1
+
+# Enter here exit cleanup code.
