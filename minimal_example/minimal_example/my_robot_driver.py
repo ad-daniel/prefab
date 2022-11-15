4f12c99db0b30cb8921f19ded710fcdf166e4348
ad-daniel <daniel.dias@epfl.ch>
2022-11-15 15:26:26 +0100

    Minimal ros2

---
+++ minimal_example/minimal_example/my_robot_driver.py
@@ -0,0 +1,20 @@
+import rclpy
+
+
+class MyRobotDriver:
+    def init(self, webots_node, properties):
+        self.__robot = webots_node.robot
+
+        self.__gps = self.__robot.getDevice('gps1')
+        self.__gyro = self.__robot.getDevice('gyro1')
+        self.__imu = self.__robot.getDevice('inertial_unit')
+
+        rclpy.init(args=None)
+        self.__node = rclpy.create_node('my_robot_driver')
+
+    def step(self):
+        rclpy.spin_once(self.__node, timeout_sec=0)
+
+        roll, pitch, yaw = self.__imu.getRollPitchYaw()
+        v1, v2, v3 = self.__gps.getValues()
+        roll_acceleration, pitch_acceleration, yaw_acceleration = self.__gyro.getValues()
